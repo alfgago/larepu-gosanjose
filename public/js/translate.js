5de67baba99502259ede6271c66b3ce339661786
Diego Carvajal <diego@5e.cr>
2022-09-09 11:50:39 -0600

    ajustes de diseño e integracion del translate

---
+++ public/js/translate.js
@@ -0,0 +1,16 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,ru,pt,fr,de,zh-CN,ja,it,en', autoDisplay: false, layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+    document.getElementsByClassName('goog-te-gadget-icon')[0].style.visibility = 'hidden';
+}
+
+
+setTimeout(() => {
+    console.log('entrada');
+    $('.goog-te-gadget-icon').remove();
+    $('.goog-te-menu-value span').first().remove();
+    $('.goog-te-menu-value img').remove();
+    $('.goog-te-menu-value span').first().remove();
+    console.log($('.goog-te-menu-value span'));   
+    $('.goog-te-menu-value').prepend('<img class="h-2 w-2" src="/assets/site/world.svg"></img>');         
+    console.log('bandera3');
+}, "500")
--- public/js/weather.js
+++ public/js/weather.js
@@ -1,9 +1,4 @@
 $.getJSON("https://api.open-meteo.com/v1/forecast?latitude=9.9325&longitude=-84.08&hourly=temperature_2m&current_weather=true", function(data) {
-
-console.log(data.current_weather.temperature);
-
-let temp = data.current_weather.temperature;
-
-$(".temp").append(temp);
-
+    let temp = data.current_weather.temperature;
+    $(".temp").append(temp);
 });

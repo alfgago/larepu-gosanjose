04038371cc2f844e8fa981e687cc0dd9422534f5
Diego Carvajal <diego@5e.cr>
2022-09-09 19:00:07 -0600

    ajustes de diseño finales y quitar  jquery

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,4 +1,32 @@
-$.getJSON("https://api.open-meteo.com/v1/forecast?latitude=9.9325&longitude=-84.08&hourly=temperature_2m&current_weather=true", function(data) {
-    let temp = data.current_weather.temperature;
-    $(".temp").append(temp);
+let getJSON = function(url, callback) {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+
+        let status = xhr.status;
+
+        if (status == 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status);
+        }
+    };
+
+    xhr.send();
+};
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+getJSON('https://api.open-meteo.com/v1/forecast?latitude=9.9325&longitude=-84.08&hourly=temperature_2m&current_weather=true',  function(err, data) {
+    if (err != null) {
+        console.error(err);
+    } else {
+        let temp = Math.trunc(data.current_weather.temperature);
+        document.getElementsByClassName("temp")[0].innerHTML=temp;
+    }
 });

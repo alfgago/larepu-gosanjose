57e2415c3c1e7ab7aa90f8d93ed9d01610aca471
Diego Carvajal <diego@5e.cr>
2022-09-20 18:30:54 -0600

    fixes y retroalimentacion de Fran en clickup - pendiente lo de los idiomas

--- public/js/translate.js
+++ public/js/translate.js
@@ -1,11 +1,17 @@
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,ru,pt,fr,de,zh-CN,ja,it,en,ko', flags:true, autoDisplay: false, layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,ru,pt,fr,de,zh-CN,ja,it,en,ko', autoDisplay: false, layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
     document.getElementsByClassName('goog-te-gadget-icon')[0].style.visibility = 'hidden';
 }
 
-function insertLg(){
+function start(){
+    let el = document.querySelectorAll('.goog-te-menu-value')[0]
+    el.addEventListener("click", function(){insertLg(el.innerText)})
+}
+
+function insertLg(lg){
     let el = document.querySelectorAll('.goog-te-menu-value > span')[0]
     const newEl = document.createElement('p')
-    newEl.innerText = 'EN'
+    newEl.classList.add('uppercase')
+    newEl.innerText = lg
     el.parentNode.insertBefore(newEl, el)
 }
